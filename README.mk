cd69fb0a5acceb17ecd18bc19a242ecbbfddc46f
aep <aep@exys.org>
2015-12-06 12:41:33 +0100

    fix incorrect liblog linkage

---
+++ README.mk
@@ -0,0 +1,8 @@
+These are various open source tools and libraries that hy5 uses
+when building android systems for customers, as well as for korhal.
+They may be helpful to others building android base systems.
+Do not try to use them in an android NDK project.
+
+The license files of the individual projects are kept intact.
+Consider our work to be licensed as whatever the original project was licensed as,
+or public domain where applicable.
